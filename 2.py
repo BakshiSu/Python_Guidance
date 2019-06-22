8b94a367d92ba90540e5edd06b970e9e3cdfd26a
BakshiSu <47112365+BakshiSu@users.noreply.github.com>
2019-06-22 15:06:48 +0530

    list operations

--- 2.py
+++ 2.py
@@ -11,3 +11,9 @@ print('\n')
 filename = input("Plase enter a filename with extension: ")
 ext = filename.split(".")
 print("The extension of the file is :" + repr(ext[-1]))
+print('\n')
+
+
+##8
+color_list = ["Red", "Green", "White", "Black"]
+print( "First element of the list: " + color_list[0] + " and last element is: " + color_list[-1])
