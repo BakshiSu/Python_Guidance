d3e115395ef2c4a70195fd5fe637f2d37e7d784c
BakshiSu <47112365+BakshiSu@users.noreply.github.com>
2019-06-24 15:17:26 +0530

    Another play workaround

--- 3.py
+++ 3.py
@@ -3,3 +3,9 @@ exam_st_date = (11, 12, 2014)
 print("The examination will start from : %i / %i / %i"%exam_st_date)
 
 ###10
+a = 5
+n1 = int("%s" %a)
+n2 = int("%s%s" %(a,a) )
+n3 = int("%s%s%s" %(a,a,a))
+y = (n1 + n2 + n3)
+print("Result is:" + str(y))
